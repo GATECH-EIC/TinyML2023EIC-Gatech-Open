97ce3038d44d4d35d3f03cbcb87efb2e186ee6ee
Xiao Luo <lxiao70@gatech.edu>
2023-10-07 22:08:04 -0400

    remove deployment/.gitignore

---
+++ Deployment/MDK-ARM/deploy2/matrix.d
@@ -0,0 +1,4 @@
+deploy2/matrix.o: ..\Core\Src\matrix.c ..\Core\Inc\matrix.h \
+  D:\ElectronicTool\Kail\Core\ARM\ARMCLANG\Bin\..\include\stdio.h \
+  D:\ElectronicTool\Kail\Core\ARM\ARMCLANG\Bin\..\include\math.h \
+  D:\ElectronicTool\Kail\Core\ARM\ARMCLANG\Bin\..\include\string.h
---
+++ Deployment/MDK-ARM/deploy2/parameter.d
@@ -0,0 +1,2 @@
+deploy2/parameter.o: ..\Core\Src\parameter.c ..\Core\Inc\parameter.h \
+  D:\ElectronicTool\Kail\Core\ARM\ARMCLANG\Bin\..\include\stdint.h
